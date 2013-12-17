24047a7a81f3ff440ec6110d220ca24ec1028bfa
Seyvoz Stéphane <stephaneseyvoz@gmail.com>
2013-12-17 18:24:25 +0100

    Fix templates for first name of C source in primitives & minor++

--- org.ow2.mindEd.ide/src/org/ow2/mindEd/ide/core/template/TemplateCompositeADL.java
+++ org.ow2.mindEd.ide/src/org/ow2/mindEd/ide/core/template/TemplateCompositeADL.java
@@ -16,7 +16,7 @@ public class TemplateCompositeADL
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "composite ";
-  protected final String TEXT_2 = " {" + NL + "  ";
+  protected final String TEXT_2 = " {" + NL + "\t";
   protected final String TEXT_3 = NL + "\t";
   protected final String TEXT_4 = NL + "}";
 
