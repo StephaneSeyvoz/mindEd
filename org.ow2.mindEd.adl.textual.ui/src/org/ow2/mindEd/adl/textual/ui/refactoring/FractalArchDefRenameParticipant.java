fa6dda7ffffab1f0082e8a3cac969bfadd6ec93a
Seyvoz Stéphane <stephane.seyvoz@non.schneider-electric.com>
2014-06-13 17:00:20 +0200

    "Rename Element" ArchDef to LTK rename & move support + MoveParticipant: - Inline renaming of an ArchitectureDefinition in the Xtext editor now able to create RenameResourceChange on short name (from FQN) modification, and MoveResourceChange on package name (from FQN) modification ; - Both at the same time cannot be supported by legacy RenameResourceChange + MoveResourceChange (we cannot chain them) so we should protect the edition to forbid both changes at the same time ; - First implementation of the "Move..." LTK Participant, inspired by Xtext's AbstractProcessorBasedRenameParticipant and our FractalFileRenameParticipant, triggering ArchitectureDefinition node modification + references.

--- org.ow2.mindEd.adl.textual.ui/src/org/ow2/mindEd/adl/textual/ui/refactoring/FractalArchDefRenameParticipant.java
+++ org.ow2.mindEd.adl.textual.ui/src/org/ow2/mindEd/adl/textual/ui/refactoring/FractalArchDefRenameParticipant.java
@@ -3,22 +3,31 @@ package org.ow2.mindEd.adl.textual.ui.refactoring;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.internal.resources.Folder;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.emf.common.util.EList;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.CompositeChange;
+import org.eclipse.ltk.core.refactoring.resource.MoveResourceChange;
 import org.eclipse.ltk.core.refactoring.resource.RenameResourceChange;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.refactoring.impl.AbstractProcessorBasedRenameParticipant;
 import org.eclipse.xtext.ui.refactoring.ui.IRenameElementContext;
 import org.eclipse.xtext.ui.util.ResourceUtil;
-import org.ow2.mindEd.adl.textual.fractal.AdlDefinition;
 import org.ow2.mindEd.adl.textual.fractal.ArchitectureDefinition;
+import org.ow2.mindEd.ide.core.ModelToProjectUtil;
+import org.ow2.mindEd.ide.model.MindProject;
 
 /**
  * This Participant is used when a "Rename Element" right-click action is used in the ADL Xtext Editor.
@@ -33,7 +42,7 @@ public class FractalArchDefRenameParticipant extends
 AbstractProcessorBasedRenameParticipant {
 
 	private ArchitectureDefinition renamedArchDef = null;
-	
+
 	/**
 	 * Do we really want to enable this Participant or not ? (is it targetting an ArchitectureDefinition ?)
 	 */
@@ -61,29 +70,96 @@ AbstractProcessorBasedRenameParticipant {
 			// don't enable this participant
 			return false;
 		}
-		
+
 		// don't enable this participant
 		return false;
 	}
 
 	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException, OperationCanceledException {
-		
+
+		// Only use if package is renamed
+		IFolder newFolder = null;
+
+		// At this point, the modification was already applied and then reverted,
+		// so our renamedArchDef has already been restored to its original name.
 		IFile file = ResourceUtil.getFile(renamedArchDef.eResource());
-		
+
+		String oldFullyQualifiedName = renamedArchDef.getName();
 		String newFullyQualifiedName = getNewName();
-		
-		int lastIndexOf = newFullyQualifiedName.lastIndexOf('.');
-		
-		// TODO: Use this to detect package changes and trigger a MoveResourceChange instead of a RenameResourceChange when needed !
-		String packageName = newFullyQualifiedName.substring(0, lastIndexOf);
-		
-		String newShortName = newFullyQualifiedName.substring(lastIndexOf + 1);
-		String fileName = newShortName + "." + file.getFileExtension();
-		
-		return new RenameResourceChange(file.getFullPath(), fileName);
+
+		int countDotsInOld = 0;
+		for (int i = 0; i < oldFullyQualifiedName.length() ; i++) {
+			if (oldFullyQualifiedName.charAt(i) == '.')
+				countDotsInOld++;
+		}
+
+		int lastIndexOfDotInOld = oldFullyQualifiedName.lastIndexOf('.');
+		int lastIndexOfDotInNew = newFullyQualifiedName.lastIndexOf('.');
+
+		// Prepare to detect package changes and trigger MoveResourceChange and / or RenameResourceChange when needed !
+		String oldPackageName 	= oldFullyQualifiedName.substring(0, lastIndexOfDotInOld);
+		String newPackageName 	= newFullyQualifiedName.substring(0, lastIndexOfDotInNew);
+
+		String oldSimpleName 	= oldFullyQualifiedName.substring(lastIndexOfDotInOld + 1);
+		String newSimpleName 	= newFullyQualifiedName.substring(lastIndexOfDotInNew + 1);
+
+		// Prepare Changes
+		CompositeChange compositeChange = new CompositeChange("Refined Changes from participant: " + getName());
+
+		// Create Model elements changes (from RenameElementProcessor) before touching the file itself
+		//Change superChanges = super.createChange(pm);
+		//compositeChange.add(superChanges);
+
+		if (!oldPackageName.equals(newPackageName)) {
+
+			URI oldURI = renamedArchDef.eResource().getURI();
+			String[] oldURIsegments = oldURI.segments();
+
+			StringBuilder newURIbuilder = new StringBuilder();
+
+			// We want to keep URI "/resource/Project/<src_path>/" without it followed by the original package and definition
+			int segmentsToKeepCount = oldURI.segmentCount() - (countDotsInOld + 1);
+
+			// Start from 1 to skip the "resource" first segment since the "createPlatformResourceURI" method will add it for us
+			for (int j = 1 ; j < segmentsToKeepCount ; j++) {
+				newURIbuilder.append(oldURIsegments[j]);
+				newURIbuilder.append("/");
+			}
+
+			// We want a target folder (not a file)
+			String newRelativePathFromNewFQN = newPackageName.replace('.', '/');
+			newURIbuilder.append(newRelativePathFromNewFQN);
+
+			IWorkspace workspace = org.eclipse.core.resources.ResourcesPlugin.getWorkspace();
+			newFolder = workspace.getRoot().getFolder(new Path(newURIbuilder.toString()));
+			if (newFolder != null) {
+				if (!newFolder.exists())
+					newFolder.create(true, false, pm);
+
+				compositeChange.add(new MoveResourceChange(file, newFolder));
+			}
+		}
+
+		// Simple rename
+		if (!oldSimpleName.equals(newSimpleName)) {
+			String oldFileName = oldSimpleName + "." + file.getFileExtension();
+			String newFileName = newSimpleName + "." + file.getFileExtension();
+			
+			IFile fileToRename = null;
+			
+			if (newFolder != null)
+				fileToRename = newFolder.getFile(oldFileName);
+			else
+				fileToRename = file;
+			
+			if (fileToRename != null)
+				compositeChange.add(new RenameResourceChange(fileToRename.getFullPath(), newFileName));
+		}
+
+		return compositeChange;
 	};
-	
+
 	/**
 	 * 
 	 */
